0e316edc288f93b6b298558a5ee40d62c467c63b
Ludovic Mermod <ludovic.mermod@gmail.com>
2024-04-16 00:21:27 +0200

    feat: add stub payload verification

--- webhook/src/main.rs
+++ webhook/src/main.rs
@@ -1,16 +1,29 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, http::StatusCode, web, App, Error, HttpMessage, HttpRequest, HttpResponse, HttpServer};
+
+mod models;
+
+fn validate_signature(req: &HttpRequest) -> bool {
+    req.headers()
+        .get("X-Hub-Signature")
+        .is_some_and(|v| v == "1234")
+}
 
 #[get("/hello/{name}")]
-async fn greet(name: web::Path<String>) -> impl Responder {
-    format!("Hello {name}!")
+async fn greet(name: web::Path<String>, req: HttpRequest) -> Result<HttpResponse<String>, Error> {
+    if !validate_signature(&req) {
+        return HttpResponse::Forbidden().message_body("Forbidden".to_owned());
+    }
+
+    Ok(HttpResponse::with_body(
+        StatusCode::OK,
+        format!("Hello {name}!"),
+    ))
 }
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new().service(greet)
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(greet))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
