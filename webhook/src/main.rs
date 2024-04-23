80c708b34f606a6b1adb6aa076dfcdce41edece1
Ludovic Mermod <ludovic.mermod@gmail.com>
2024-04-24 00:20:49 +0200

    fix: update webhook port

--- webhook/src/main.rs
+++ webhook/src/main.rs
@@ -11,13 +11,13 @@ mod validation;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
-    log::info!("Starting webhook on 127.0.0.1:8080");
+    log::info!("Starting webhook on 127.0.0.1:4000");
 
     // Load the config
     config();
 
     HttpServer::new(|| App::new().service(generic).service(targeted))
-        .bind(("127.0.0.1", 8080))?
+        .bind(("127.0.0.1", 4000))?
         .run()
         .await
 }
